fdd2a64c4ccdc8a22089e53d4c8e1e2b3a20a4cf
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2023-03-07 09:27:31 -0800

    Add files via upload

--- memdump.cpp
+++ memdump.cpp
@@ -77,66 +77,66 @@ void worker_thread() {
         {
             std::unique_lock<std::mutex> lock(g_mutex);
             // Wait for a signal that there is work to be done or that all work is complete
-            g_cv.wait(lock, []{ return !dumpQueue.empty() || dumpedPids.size() >= 1024; });
-            if (dumpQueue.empty() && dumpedPids.size() >= 1024) {
-                // Stop processing new tasks if we've dumped the memory of all processes
-                numFinishedThreads++;
-                if (numFinishedThreads == numThreads) {
-std::cout << "Memory of all processes dumped, exiting program..." << std::endl;
-return;
+            g_cv.wait(lock, []{ return !dumpQueue.empty() || numFinishedThreads == numThreads; });
+            if (dumpQueue.empty() && numFinishedThreads == numThreads) {
+                // Exit program if we've dumped the memory of all non-system and non-own processes
+                return;
+            }
+            // Get the next process ID from the queue
+            processId = dumpQueue.front();
+            dumpQueue.pop();
+        }
+        dumpMemory(processId);
+    }
 }
-continue;
-}
-// Get the next process ID from the queue
-processId = dumpQueue.front();
-dumpQueue.pop();
-}
-dumpMemory(processId);
-}
-}
+
 
 int main() {
-// Get the list of process IDs
-std::vector<DWORD> processIds(1024);
-DWORD bytesReturned;
-while (true) {
-if (EnumProcesses(processIds.data(), processIds.size() * sizeof(DWORD), &bytesReturned) == FALSE) {
-std::cerr << "Error: could not enumerate processes" << std::endl;
-return 1;
-}
-if (bytesReturned < processIds.size() * sizeof(DWORD)) {
-processIds.resize(bytesReturned / sizeof(DWORD));
-break;
-}
-processIds.resize(processIds.size() * 2);
-}
-// Remove the system processes from the list
-processIds.erase(std::remove_if(processIds.begin(), processIds.end(), [](DWORD processId) {
-    // Open a handle to the process
-    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, processId);
-    if (hProcess == NULL) {
-        return true;
+    // Get the list of process IDs
+    std::vector<DWORD> processIds(1024);
+    DWORD bytesReturned;
+    while (true) {
+        if (EnumProcesses(processIds.data(), processIds.size() * sizeof(DWORD), &bytesReturned) == FALSE) {
+            std::cerr << "Error: could not enumerate processes" << std::endl;
+            return 1;
+        }
+        if (bytesReturned < processIds.size() * sizeof(DWORD)) {
+            processIds.resize(bytesReturned / sizeof(DWORD));
+            break;
+        }
+        processIds.resize(processIds.size() * 2);
     }
 
-    // Get the name of the process
-    TCHAR processName[MAX_PATH];
-    if (GetModuleFileNameEx(hProcess, NULL, processName, MAX_PATH) == 0) {
+    // Get the program's own process ID
+    DWORD ownProcessId = GetCurrentProcessId();
+
+    // Remove the system and own processes from the list
+    processIds.erase(std::remove_if(processIds.begin(), processIds.end(), [ownProcessId](DWORD processId) {
+        // Open a handle to the process
+        HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, processId);
+        if (hProcess == NULL) {
+            return true;
+        }
+
+        // Get the name of the process
+        TCHAR processName[MAX_PATH];
+        if (GetModuleFileNameEx(hProcess, NULL, processName, MAX_PATH) == 0) {
+            CloseHandle(hProcess);
+            return true;
+        }
         CloseHandle(hProcess);
-        return true;
+
+        // Check if the process is a system process or the own process
+        return processId == ownProcessId || PathIsSystemFolderA(processName, FILE_ATTRIBUTE_DIRECTORY) != FALSE;
+
+    }), processIds.end());
+
+    // Dump the memory of the non-system and non-own processes
+    for (DWORD processId : processIds) {
+        dumpQueue.push(processId);
     }
-    CloseHandle(hProcess);
 
-    // Check if the process is a system process
-    return PathIsSystemFolderA(processName, FILE_ATTRIBUTE_DIRECTORY) != FALSE;
-
-}), processIds.end());
-
-// Dump the memory of the non-system processes
-for (DWORD processId : processIds) {
-    dumpQueue.push(processId);
-}
-
-// Create and start the worker threads
+    // Create and start the worker threads
 std::vector<std::thread> workerThreads;
 for (int i = 0; i < numThreads; i++) {
     workerThreads.emplace_back(worker_thread);
@@ -145,7 +145,11 @@ for (int i = 0; i < numThreads; i++) {
 // Wait for the worker threads to finish
 for (std::thread& thread : workerThreads) {
     thread.join();
+    numFinishedThreads++;
 }
+
+std::cout << "Memory of all non-system and non-own processes dumped, exiting program..." << std::endl;
 
 return 0;
 }
+
